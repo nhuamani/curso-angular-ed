38b5b1dd1deef2ad4a78f982b774f97c410b2135
nhuamani <nhuamani.c7@gmail.com>
2024-11-11 15:55:04 -0500

    doc: Two-way binding with form controls

---
+++ src/app/databinding/databinding.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-databinding',
+  templateUrl: './databinding.component.html',
+  styleUrl: './databinding.component.scss'
+})
+export class DatabindingComponent {
+
+  // Interpolación
+  course: string = 'Curso';
+
+
+  // Property Binding
+
+  cardData = {
+    imageUrl: 'https://placehold.co/400',
+    title: 'Angular',
+    description: 'Curso desde cero'
+  }
+
+  // Event Binding
+
+  onChangeTitle(event: any): void {
+    this.cardData.title = event.target.value;
+  }
+
+  changeTitle(): void {
+    this.cardData.title = "Python";
+  }
+
+}
