f637c5f8da0de510dcb79424c2f092836ccaf48d
nhuamani <nhuamani.c7@gmail.com>
2024-11-12 15:55:21 -0500

    doc: Routes with Components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DatabindingComponent } from './databinding/databinding.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: 'data', component: DatabindingComponent }
+  { path: '', component: HomeComponent },
+  { path: 'data', component: DatabindingComponent },
+  { path: '**', redirectTo: '/' }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,19 @@ import { DatabindingComponent } from './databinding/databinding.component';
 import { FormsModule } from '@angular/forms';
 import { CounterComponent } from './counter/counter.component';
 import { CalculatorComponent } from './calculator/calculator.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DatabindingComponent,
     CounterComponent,
-    CalculatorComponent
+    CalculatorComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
